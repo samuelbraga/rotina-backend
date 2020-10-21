476dde49698f455c91be652205113bf2d0383376
Samuel Braga <samuel.fantini.braga@hotmail.com>
2020-10-21 20:30:51 -0300

    Initial commit

---
+++ src/main/java/com/samuelbraga/rotinabackend/RotinaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.samuelbraga.rotinabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotinaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotinaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelbraga/rotinabackend/RotinaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelbraga.rotinabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotinaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
